533784ecdb492d67269932e7b0007d3911a1e5f3
dman14 <darius.chira@yahoo.com>
2021-05-27 12:21:41 +0000

    "typo"

--- models/sr_vdvae_1.py
+++ models/sr_vdvae_1.py
@@ -54,7 +54,7 @@ class SRVAE(nn.Module):
 
   def forward(self, lr, hr):
     activations_sr = self.vae_sr.forward_sr_activations(lr)
-    hr_proc = self.preprocess_func(hr)
+    hr, hr_proc = self.preprocess_func(hr)
     stats = self.vae.forward(hr, hr_proc, activations_sr=activations_sr)
     return stats
         
