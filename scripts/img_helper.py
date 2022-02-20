2e132ee2646d221ff15b33abd6f4a8fc981aa89a
Darius Chira <darius.chira@yahoo.com>
2022-02-20 13:29:59 +0100

    new ssim function from basicsr

--- scripts/img_helper.py
+++ scripts/img_helper.py
@@ -270,9 +270,41 @@ def quality_measure_YCbCr(target, output):
   y2 = output_ycbcr[:,:,0]
  
   psnr = cv2.PSNR(y1, y2) 
-  score, diff = ssim(y1.astype(np.uint8), y2.astype(np.uint8), multichannel= False, full = True)
+  #score, diff = ssim(y1.astype(np.uint8), y2.astype(np.uint8), multichannel= False, full = True)
+  score = ssim_basicsr(y1, y2)
 
   return psnr, score
+
+
+def ssim_basicsr(img, img2):
+  """Calculate SSIM (structural similarity) for one channel images.
+  It is called by func:`calculate_ssim`.
+  Args:
+      img (ndarray): Images with range [0, 255] with order 'HWC'.
+      img2 (ndarray): Images with range [0, 255] with order 'HWC'.
+  Returns:
+      float: ssim result.
+    """
+
+  c1 = (0.01 * 255)**2
+  c2 = (0.03 * 255)**2
+
+  img = img.astype(np.float64)
+  img2 = img2.astype(np.float64)
+  kernel = cv2.getGaussianKernel(11, 1.5)
+  window = np.outer(kernel, kernel.transpose())
+
+  mu1 = cv2.filter2D(img, -1, window)[5:-5, 5:-5]
+  mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+  mu1_sq = mu1**2
+  mu2_sq = mu2**2
+  mu1_mu2 = mu1 * mu2
+  sigma1_sq = cv2.filter2D(img**2, -1, window)[5:-5, 5:-5] - mu1_sq
+  sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+  sigma12 = cv2.filter2D(img * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+  ssim_map = ((2 * mu1_mu2 + c1) * (2 * sigma12 + c2)) / ((mu1_sq + mu2_sq + c1) * (sigma1_sq + sigma2_sq + c2))
+  return ssim_map.mean()
 
 #Making Patches
 def make_patches(tensor,patch_size=16,scale=4):
